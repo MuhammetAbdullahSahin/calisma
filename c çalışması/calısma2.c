3a1f61d4359e256935970c4973091c3098ee7264
MuhammetAbdullahSahin <21060622@stu.omu.edu.tr>
2022-06-15 19:57:42 +0300

    main

--- c çalışması/calısma2.c
+++ c çalışması/calısma2.c
@@ -4,7 +4,7 @@
 #include <math.h>
 #include <string.h>
 
-struct ogrenciler()
+struct ogrenciler
 {
         char isim[20];
         char soyisim[20];
@@ -21,13 +21,21 @@ int main()
         char ogrenci_bilgileri[4][2];
         struct ogrenciler ogrenci_1;
 
-                strcpy(ogrenci_1.isim,"apo");
-                strcpy(ogrenci_1.soyisim,"sahin");
-                ogrenci_1.numara,22;
-                ogrenci_1.yas,19;
-
-        printf("%s %s %d %d",ogrenci_1.isim,ogrenci_1.soyisim,ogrenci_1.numara,ogrenci_1.yas);
+        strcpy(ogrenci_1.isim,"apo");
+        strcpy(ogrenci_1.soyisim,"sahin");
+        ogrenci_1.numara = 22;
+        ogrenci_1.yas = 19;
         
+        struct ogrenciler ogrenci_2;
+
+        strcpy(ogrenci_2.isim,"czn");
+        strcpy(ogrenci_2.soyisim,"kla");
+        ogrenci_2.numara = 26;
+        ogrenci_2.yas = 19;
+
+
+        printf("%s %s %d %d\n",ogrenci_1.isim,ogrenci_1.soyisim,ogrenci_1.numara,ogrenci_1.yas);
+        printf("%s %s %d %d",ogrenci_2.isim,ogrenci_2.soyisim,ogrenci_2.numara,ogrenci_2.yas);
         
         return 0;
 }
